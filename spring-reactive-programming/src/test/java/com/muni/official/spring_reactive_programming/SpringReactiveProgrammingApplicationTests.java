2766c121d00d40ec1f26294737eed67ddbad70a2
Chsudheer2444 <sudheerdev8@gmail.com>
2025-04-12 13:27:43 +0530

    initial commit

---
+++ spring-reactive-programming/src/test/java/com/muni/official/spring_reactive_programming/SpringReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muni.official.spring_reactive_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
